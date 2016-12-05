a7c33cf94b493bbd7c7b939084f98efa93cda799
Kevin Wang <kevmo314@gmail.com>
2016-12-05 02:41:58 -0500

    Clean up UI, add LICENSE.md

--- ui/e2e/app.e2e-spec.ts
+++ ui/e2e/app.e2e-spec.ts
@@ -1,15 +1,14 @@
-import { browser, element, by } from 'protractor';
+import { MyprojectPage } from './app.po';
 
-describe('QuickStart E2E Tests', function () {
+describe('myproject App', function() {
+  let page: MyprojectPage;
 
-  let expectedMsg = 'Hello Angular';
-
-  beforeEach(function () {
-    browser.get('');
+  beforeEach(() => {
+    page = new MyprojectPage();
   });
 
-  it('should display: ' + expectedMsg, function () {
-    expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
   });
-
 });
