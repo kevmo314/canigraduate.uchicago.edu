0eafd24ecb5f80b0bf013fadd4c867f068827bc4
Kevin Wang <kevmo314@gmail.com>
2017-03-25 13:57:04 -0400

    Add transition to course name. Remove unnecessary wrapper.

--- frontend/src/app/course-info/course-name.component.ts
+++ frontend/src/app/course-info/course-name.component.ts
@@ -1,20 +1,26 @@
 import { DatabaseService } from '../database/database.service';
-import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy, HostBinding, AfterViewInit } from '@angular/core';
 
 const UNKNOWN_COURSE_NAME = 'Unknown';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'cig-course-name',
-  template: `<span [class.unknown]="(name | async) == '${UNKNOWN_COURSE_NAME}'">{{name | async}}</span>`,
-  styles: ['.unknown { font-style: italic; opacity: 0.54; }']
+  template: `{{name | async}}`,
+  styleUrls: ['./course-name.component.scss']
 })
-export class CourseNameComponent {
+export class CourseNameComponent implements AfterViewInit {
+  @HostBinding('class.unknown') unknown = false;
   @Input() course: string;
-  constructor(private databaseService: DatabaseService) {}
+
+  constructor(private databaseService: DatabaseService) {  }
+
+  ngAfterViewInit() {
+    this.name.subscribe(name => this.unknown = (name === UNKNOWN_COURSE_NAME));
+  }
 
   get name() {
     return this.databaseService.name(this.course)
-        .map(x => x || UNKNOWN_COURSE_NAME);
+        .map(name => name || UNKNOWN_COURSE_NAME);
   }
 }
