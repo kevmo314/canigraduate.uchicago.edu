091a6d3706712173eeba0199bcb1b915e1df8b47
Kevin Wang <kevmo314@gmail.com>
2017-05-09 04:32:24 +0000

    scraper updates

--- backend/uchicago/scraper.py
+++ backend/uchicago/scraper.py
@@ -2,6 +2,7 @@ import collections
 import pyrebase
 import json
 import httplib2
+import re
 
 from src import Term
 
@@ -16,12 +17,20 @@ firebase = pyrebase.initialize_app({
 def transform(a):
     return dict([(i, j) for i, j in enumerate(a) if j is not None])
 
+alphanumeric = re.compile('[^a-z0-9 ]+')
+
+def get_words(text):
+    return set(alphanumeric.sub('', text.lower()).split())
+
 def rebuild_indexes(db):
     schedules = db.child('schedules').get().val()
+    course_info = db.child('course-info').get().val()
     instructors = collections.defaultdict(set)
     departments = collections.defaultdict(set)
     periods = collections.defaultdict(set)
+    terms = collections.defaultdict(set)
     intervals = collections.defaultdict(set)
+    inverted = collections.defaultdict(set)
     for course_id, a in schedules.items():
         for year, b in a.items():
             for period, c in b.items():
@@ -29,18 +38,30 @@ def rebuild_indexes(db):
                     # Deal with Firebase's array heuristic.
                     c = transform(c)
                 for id, section in c.items():
-                    for section in section['primaries'] + section['secondaries'].values():
-                        for instructor in section.instructors:
+                    for section in section['primaries'] + list(section.get('secondaries', {}).values()):
+                        for instructor in section.get('instructors', []):
                             instructors[instructor].add(course_id)
                     departments[course_id[:4]].add(course_id)
                     periods[period].add(course_id)
+                    terms['%s %s' % (period, year)].add(course_id)
+                    # Construct the inverted index based on the course description and name.
+                    info = course_info.get(course_id, {})
+                    name = info.get('name', '')
+                    description = info.get('description', '')
+                    print(name)
+                    for word in get_words(course_id) | get_words(name) | get_words(description):
+                        inverted[word].add(course_id)
                     # Firebase doesn't store empty arrays.
                     for schedule in section.get('schedule', []):
                         intervals['%d-%d' % (schedule[0], schedule[1])].add(course_id)
-    db.child('indexes').child('instructors').set(dict([(a, list(b)) for a, b in instructors.items()]))
-    db.child('indexes').child('departments').set(dict([(a, list(b)) for a, b in departments.items()]))
-    db.child('indexes').child('periods').set(dict([(a, list(b)) for a, b in periods.items()]))
-    db.child('indexes').child('schedules').set(dict([(a, list(b)) for a, b in intervals.items()]))
+    def flatten_set(d):
+        return dict([(a, list(b)) for a, b in d.items()])
+    db.child('indexes').child('fulltext').set(flatten_set(inverted))
+    db.child('indexes').child('instructors').set(flatten_set(instructors))
+    db.child('indexes').child('departments').set(flatten_set(departments))
+    db.child('indexes').child('periods').set(flatten_set(periods))
+    db.child('indexes').child('terms').set(flatten_set(terms))
+    db.child('indexes').child('schedules').set(flatten_set(intervals))
     db.child('indexes').child('all').set(list(schedules.keys()))
 
 def scrape_data(db):
@@ -69,10 +90,10 @@ def scrape_data(db):
                     'notes': section.notes,
                     'enrollment': section.enrollment,
                     'primaries': [{
-                        'instructors': secondary.instructors,
-                        'schedule': secondary.schedule,
-                        'type': secondary.type,
-                        'location': secondary.location
+                        'instructors': primary.instructors,
+                        'schedule': primary.schedule,
+                        'type': primary.type,
+                        'location': primary.location
                     } for primary in section.primaries],
                     'secondaries': dict([(secondary.id, {
                         'instructors': secondary.instructors,
@@ -93,6 +114,6 @@ def scrape_data(db):
 
 if __name__ == '__main__':
     db = firebase.database()
-    db.child('schedules').set({})
-    scrape_data(db)
-    # rebuild_indexes(db)
+    # db.child('schedules').set({})
+    # scrape_data(db)
+    rebuild_indexes(db)
