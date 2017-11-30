595c5a8b03a6219509b328bbfaf6048dbbe7897b
Kevin Wang <kevmo314@gmail.com>
2017-11-30 02:49:17 -0500

    Add EGPA calculations.

--- vue-frontend/src/components/Sidebar.vue
+++ vue-frontend/src/components/Sidebar.vue
@@ -9,18 +9,24 @@
         </v-tabs-bar>
         <v-tabs-items>
           <v-tabs-content id="transcript" class="content">
+            <div class="caption text-xs-center px-3 py-2">
+              <strong class="grey--text text--darken-2">Your GPA is bold.</strong> <span class="grey--text">The average GPA is in grey.</span>
+            </div>
             <v-expansion-panel focusable>
               <v-expansion-panel-content v-for="(term, index) in terms" :key="term" :value="index === terms.length - 1">
                 <div slot="header" class="header display-flex" align-center>
                   {{term}}
-                  <div class="flex-grow caption grey--text text-xs-right mr-3">
+                  <div class="flex-grow caption grey--text text-xs-right">
                     <span v-if="transcript.find(t => t.term == term && t.quality)">
                       Quarter
-                      <strong class="grey--text text--darken-2">{{getQuarterGpa(term).toFixed(2)}}</strong>&nbsp;&middot;&nbsp;
+                      <strong class="grey--text text--darken-2">{{quarterGpa[index].toFixed(2)}}</strong>
+                      <span v-if="quarterEgpa" class="grey--text">{{quarterEgpa[index].toFixed(2)}}</span>
+                      &nbsp;&middot;&nbsp;
                     </span>
                     <span v-if="transcript.find(t => t.quality)">
                       Cumulative
-                      <strong class="grey--text text--darken-2">{{getCumulativeGpa(term).toFixed(2)}}</strong>
+                      <strong class="grey--text text--darken-2">{{cumulativeGpa[index].toFixed(2)}}</strong>
+                      <span v-if="cumulativeEgpa" class="grey--text">{{cumulativeEgpa[index].toFixed(2)}}</span>
                     </span>
                   </div>
                 </div>
@@ -33,8 +39,9 @@
                         <course-name>{{record.course}}</course-name>
                       </v-list-tile-sub-title>
                     </v-list-tile-content>
-                    <v-list-tile-avatar class="grey--text text--darken-4" v-if="record.complete">
-                      {{record.quality ? record.gpa.toFixed(1) : record.grade}}
+                    <v-list-tile-avatar v-if="record.complete" class="display-flex mr-2 py-3">
+                      <span class="grey--text text--darken-4">{{record.quality ? record.gpa.toFixed(1) : record.grade}}</span>
+                      <span v-if="record.quality" class="grey--text ml-2">{{egpa[record.course].toFixed(1)}}</span>
                     </v-list-tile-avatar>
                   </v-list-tile>
                 </v-list>
@@ -57,10 +64,20 @@
 import CourseName from '@/components/CourseName.vue';
 import Calendar from '@/components/Calendar.vue';
 import { mapState, mapActions, mapMutations } from 'vuex';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/combineLatest';
 import EventBus from '@/EventBus';
 
 function mean(x) {
-  return x.map(record => record.gpa).reduce((a, b) => a + b, 0) / x.length;
+  return x.reduce((a, b) => a + b, 0) / x.length;
+}
+
+function frequencyMean(x) {
+  const count = Object.values(x).reduce((a, b) => a + b, 0);
+  const total = Object.keys(x)
+    .map(key => parseFloat(key) * x[key])
+    .reduce((a, b) => a + b, 0);
+  return total / count;
 }
 
 export default {
@@ -79,34 +96,86 @@ export default {
       temporaryTerm: state => state.temporary.term,
       scheduleTerm: state => state.temporary.term || state.activeTerm,
     }),
+    quarterGpa() {
+      const quality = this.transcript.filter(record => record.quality);
+      const qualityTerms = quality.map(record => record.term);
+      return this.terms.map(term => {
+        const from = qualityTerms.indexOf(term);
+        const to = qualityTerms.lastIndexOf(term) + 1;
+        return mean(quality.slice(from, to).map(record => record.gpa));
+      });
+    },
+    cumulativeGpa() {
+      const quality = this.transcript.filter(record => record.quality);
+      const qualityTerms = quality.map(record => record.term);
+      return this.terms.map(term => {
+        const to = qualityTerms.lastIndexOf(term) + 1;
+        return mean(quality.slice(0, to).map(record => record.gpa));
+      });
+    },
   },
   data() {
-    return { active: null }
+    return { active: null };
   },
   mounted() {
-    EventBus.$on('show-schedule-tab', () => this.active = 'schedule')
+    EventBus.$on('show-schedule-tab', () => (this.active = 'schedule'));
   },
   methods: {
     ...mapActions('filter', ['reset']),
     ...mapMutations('calendar', ['setActiveTerm']),
-    getQuarterGpa(term) {
-      return mean(this.transcript.filter(record => record.term == term && record.quality));
-    },
-    getCumulativeGpa(term) {
-      const qualityRecords = this.transcript.filter(record => record.quality);
-      return mean(qualityRecords.slice(0, qualityRecords.map(record => record.term).lastIndexOf(term)));
-    },
   },
   subscriptions() {
+    const quality = this.$watchAsObservable(() => this.transcript, {
+      immediate: true,
+    })
+      .map(x => x.newValue)
+      .map(transcript => transcript.filter(record => record.quality));
+    const terms = this.$watchAsObservable(() => this.terms, {
+      immediate: true,
+    }).map(x => x.newValue);
+    const egpa = this.endpoints.gradeDistribution().map(gradeDistribution => {
+      return Object.keys(gradeDistribution).reduce(
+        (state, key) => ({
+          ...state,
+          [key]: frequencyMean(gradeDistribution[key]),
+        }),
+        {},
+      );
+    });
     return {
       allTerms: this.endpoints.terms().do(terms => {
         if (!this.$store.state.calendar.activeTerm) {
           this.$store.commit('calendar/setActiveTerm', terms[0]);
         }
-      })
-    }
-  }
-}
+      }),
+      quarterEgpa: Observable.combineLatest(
+        quality,
+        egpa,
+        terms,
+      ).map(([quality, egpa, terms]) => {
+        const qualityTerms = quality.map(record => record.term);
+        const grades = quality.map(record => egpa[record.course]);
+        return terms.map(term => {
+          const from = qualityTerms.indexOf(term);
+          const to = qualityTerms.lastIndexOf(term) + 1;
+          return mean(grades.slice(from, to));
+        });
+      }),
+      cumulativeEgpa: Observable.combineLatest(
+        quality,
+        egpa,
+        terms,
+      ).map(([quality, egpa, terms]) => {
+        const qualityTerms = quality.map(record => record.term);
+        const grades = quality.map(record => egpa[record.course]);
+        return terms.map(term => {
+          return mean(grades.slice(0, qualityTerms.lastIndexOf(term) + 1));
+        });
+      }),
+      egpa,
+    };
+  },
+};
 </script>
 
 <style scoped>
@@ -117,5 +186,9 @@ export default {
 .content {
   max-height: calc(100vh - 150px);
   overflow-y: auto;
+}
+
+.content>>>.header__icon {
+  display: none;
 }
 </style>
