250aed1f745f5dd2267df7a2b96e1d71c9d4562a
Kevin Wang <kevmo314@gmail.com>
2018-05-21 23:20:45 -0400

    Delete old scraper code.

--- backend/uchicago/src/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import timeschedules
-from . import coursesearch
-from .course import Course
-from .term import Term
-from .collegecatalog_parser import CollegeCatalogParser
-from .ribbit_parser import RibbitParser
