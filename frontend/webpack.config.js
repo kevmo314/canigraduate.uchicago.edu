09130618ba5a6c48f1578fc6dff4ec81560a9063
Kevin Wang <kevmo314@gmail.com>
2018-05-23 00:15:29 -0400

    Update tsconfig and format.

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,69 +1,70 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
-const history = require('connect-history-api-fallback');
-const convert = require('koa-connect');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+const history = require("connect-history-api-fallback");
+const convert = require("koa-connect");
 
 module.exports = {
   mode: process.env.NODE_ENV,
   devtool:
-    process.env.NODE_ENV == 'development' ? 'eval-source-map' : 'source-map',
-  entry: ['@babel/polyfill', './src/index.js'],
+    process.env.NODE_ENV == "development" ? "eval-source-map" : "source-map",
+  entry: ["@babel/polyfill", "./src/index.js"],
   resolve: {
-    extensions: ['.js', '.ts', '.vue', '.json'],
+    extensions: [".js", ".ts", ".vue", ".json"],
     alias: {
-      vue$: 'vue/dist/vue.esm.js',
-      '@': __dirname + '/src',
-    },
+      vue$: "vue/dist/vue.esm.js",
+      "@": __dirname + "/src"
+    }
   },
   module: {
     rules: [
       {
         test: /\.vue$/,
-        loader: 'vue-loader',
+        loader: "vue-loader",
+        options: { loaders: { ts: "babel-loader" } }
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
       },
       {
         test: /\.(js|ts)$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: "babel-loader"
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        loader: 'url-loader',
+        loader: "url-loader",
         options: {
           limit: 10000,
-          name: 'static/img/[name].[hash:7].[ext]',
-        },
+          name: "static/img/[name].[hash:7].[ext]"
+        }
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url-loader',
+        loader: "url-loader",
         options: {
           limit: 10000,
-          name: 'static/fonts/[name].[hash:7].[ext]',
-        },
-      },
-    ],
+          name: "static/fonts/[name].[hash:7].[ext]"
+        }
+      }
+    ]
   },
-  output: { publicPath: '/' },
+  output: { publicPath: "/" },
   plugins: [
-    new webpack.EnvironmentPlugin(['NODE_ENV']),
+    new webpack.EnvironmentPlugin(["NODE_ENV"]),
     new HtmlWebpackPlugin({
-      template: 'src/index.html',
-      inject: true,
+      template: "src/index.html",
+      inject: true
     }),
     new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css',
-    }),
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
   ],
   serve: {
     add(app, middleware, options) {
       app.use(convert(history()));
-    },
-  },
+    }
+  }
 };
