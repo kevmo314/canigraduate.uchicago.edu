13ea1de3a373c241b80d66622ec820338a27f316
Alvin Zhao <alvinz87@gmail.com>
2018-06-07 23:48:58 -0400

    Added watches functionality: watch class, watches class, watchdeserializer class.
    First commit woot!

--- backend/uchicago/scraper/src/main/java/com/canigraduate/uchicago/firestore/Watches.java
+++ backend/uchicago/scraper/src/main/java/com/canigraduate/uchicago/firestore/Watches.java
@@ -3,8 +3,12 @@ package com.canigraduate.uchicago.firestore;
 import com.canigraduate.uchicago.deserializers.WatchDeserializer;
 import com.canigraduate.uchicago.firestore.models.Document;
 import com.canigraduate.uchicago.models.Watch;
+import com.google.common.collect.Streams;
 
+import java.util.AbstractMap;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Watches {
 
@@ -26,5 +30,12 @@ public class Watches {
                 .map(Document::getFields)
                 .filter(fields -> fields.get("course").isPresent())
                 .map(WatchDeserializer::fromMapValue);
+    }
+
+    public Iterable<Map.Entry<String, Watch>> all() {
+        return Streams.stream(this.root.allDocuments())
+                .map(doc -> new AbstractMap.SimpleImmutableEntry<>(doc.getId(),
+                        WatchDeserializer.fromMapValue(doc.getFields())))
+                .collect(Collectors.toList());
     }
 }
