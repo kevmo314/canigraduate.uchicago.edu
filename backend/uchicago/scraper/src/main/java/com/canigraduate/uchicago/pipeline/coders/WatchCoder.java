8483b0d3fb3fdd29b956d0ab1a5359522ec1eafd
Alvin Zhao <alvinz87@gmail.com>
2018-06-08 00:29:32 -0400

    Added WatchCoder functionality

--- backend/uchicago/scraper/src/main/java/com/canigraduate/uchicago/pipeline/coders/WatchCoder.java
+++ backend/uchicago/scraper/src/main/java/com/canigraduate/uchicago/pipeline/coders/WatchCoder.java
@@ -10,7 +10,6 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public class WatchCoder extends CustomCoder<Watch> {
-    // for the git so stupiasdfjlzxjckjcvlzd
     private static final Coder<String> STRING_CODER = StringUtf8Coder.of();
 
 
