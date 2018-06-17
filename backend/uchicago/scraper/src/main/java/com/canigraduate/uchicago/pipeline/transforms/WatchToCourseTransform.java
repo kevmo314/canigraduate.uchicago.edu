543dda5ad8bf7d4eb302367013a8bfa423e5f8ae
Kevin Wang <kevmo314@gmail.com>
2018-06-17 13:17:49 -0400

    Update course in termkey.

--- backend/uchicago/scraper/src/main/java/com/canigraduate/uchicago/pipeline/transforms/WatchToCourseTransform.java
+++ backend/uchicago/scraper/src/main/java/com/canigraduate/uchicago/pipeline/transforms/WatchToCourseTransform.java
@@ -14,19 +14,18 @@ import org.apache.beam.sdk.values.TypeDescriptors;
 
 import java.util.stream.Collectors;
 
-public class WatchToCourseTransform extends
-        PTransform<PCollection<KV<String, Watch>>, PCollection<KV<TermKey, Course>>> {
+public class WatchToCourseTransform
+        extends PTransform<PCollection<KV<String, Watch>>, PCollection<KV<TermKey, Course>>> {
     @Override
     public PCollection<KV<TermKey, Course>> expand(PCollection<KV<String, Watch>> input) {
-        return input.apply(FlatMapElements
-                .into(TypeDescriptors.kvs(TypeDescriptor.of(TermKey.class), TypeDescriptor.of(Course.class))).via(
-                        // e is type Map<String, Watch>
-                        e ->
-                                CourseSearch.getCoursesFromWatch(e.getValue()).entrySet().stream()
-                                        // a is type Map<String, Course>
-                                        .map(a -> KV.of(TermKey.builder().setTerm(Term.create(e.getValue()
-                                                        .getTerm())).setCourse(e.getValue().getCourse()).build(),
-                                                a.getValue())).collect(Collectors.toList())
-                ));
+        return input.apply(FlatMapElements.into(
+                TypeDescriptors.kvs(TypeDescriptor.of(TermKey.class), TypeDescriptor.of(Course.class))).via(
+                // e is type Map<String, Watch>
+                e -> CourseSearch.getCoursesFromWatch(e.getValue()).entrySet().stream()
+                        // a is type Map<String, Course>
+                        .map(a -> KV.of(TermKey.builder()
+                                .setTerm(Term.create(e.getValue().getTerm()))
+                                .setCourse(a.getKey())
+                                .build(), a.getValue())).collect(Collectors.toList())));
     }
 }
