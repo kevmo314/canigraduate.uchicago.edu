591afdcac1795f88579a3f2fd6398d2690c32b7c
Kevin Wang <kevmo314@gmail.com>
2018-06-27 00:57:57 -0400

    Remove affiliation.

--- frontend/src/components/About.vue
+++ frontend/src/components/About.vue
@@ -52,12 +52,11 @@
         </p>
         <p>Interested in contributing? Tired of seeing stuff unfinished? Check out our
           <a href="http://github.com/kevmo314/canigraduate.uchicago.edu">GitHub page</a> and contribute!</p>
-        <p>No idea where to start? Email Kevin at <a href="mailto:kevmo314@gmail.com">kevmo314@gmail.com</a>.</p>
+        <p>No idea where to start? Email <a href="mailto:kevmo314@gmail.com">kevmo314@gmail.com</a>.</p>
       </v-card-text>
       <v-card-media>
         <v-layout column text-xs-center>
           <v-divider></v-divider>
-          <div class="my-2">This site is not affiliated with {{ institutionName }}</div>
           <div class="my-2">©{{year}} — <strong>Can I Graduate?</strong></div>
         </v-layout>
       </v-card-media>
